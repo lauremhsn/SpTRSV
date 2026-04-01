90719e14ba210fe63218f602f0280ad3a666b74a
lauremhsn <43182760+lauremhsn@users.noreply.github.com>
2026-04-01 23:44:10 +0300

    Update and rename kernel0.cu to kernelCPU0.cu

--- kernel0.cu
+++
@@ -1,7 +0,0 @@
-#include "common.h"
-
-
-void sptrsv_gpu0(CSCMatrix* L_c, CSRMatrix* L_r, DenseMatrix* B, DenseMatrix* X, CSCMatrix* L_c_host, CSRMatrix* L_r_host, unsigned int numCols){
-    
-
-}
---
+++ kernelCPU0.cu
@@ -0,0 +1,7 @@
+#include "common.h"
+
+
+void sptrsv_cpu(CSRMatrix* L, DenseMatrix* B, DenseMatrix* X){
+    
+
+}
